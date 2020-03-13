20f596c9eecc05b02cdc2014fd5f872eb5059c58
Rayven <rayven.burns@gmail.com>
2020-03-13 11:53:32 -0400

    ex1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -10,9 +10,27 @@ def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
     """
-    YOUR CODE HERE
+    Finds two items whose sum of weights equals the weight limit.
+    Answer looks like -> (zero, one)
+    Each element represents the item weights (index position) of the two packages
+    HIGHER_NUM[0],SMALLER_NUM[1]
+    If pair doesn't exist for the given inputs return None
+    !Note!: If the key doesn't exist in the ht, ht_retrieve returns None
     """
+    #Based off of last hint, loop through weights and store each weight's list index as its value
 
+    #loop through weights
+    for i in range(len(weights)):
+        #run insert to add: it takes ht, key and value
+        hash_table_insert(ht, weights[i], i)
+    #loop through weights again 
+    for i in range(len(weights)):
+        #check to see if entry for limit-weight
+        entry = hash_table_retrieve(ht, limit-weights[i])
+        #if there is
+        if entry:
+            #return it, entry is larger..i is smaller
+            return (entry, i)
     return None
 
 
