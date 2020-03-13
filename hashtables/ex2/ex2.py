0a771726158a36887db90376f835c3a0a3ad529f
Rayven <rayven.burns@gmail.com>
2020-03-13 12:54:17 -0400

    ex2 done, hashtables done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,10 +14,35 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = [None] * (length - 1) #gets rid of extra element at end the(NONE)
 
     """
-    YOUR CODE HERE
+    Objective: reconstruct trip from the tickets
+    Source string is starting point 
+    Destination string is next airport along trip
+    Ticket for first & final flight has destination with source = None 
+    The function should output an array of strings with entire route of trip
+    Can assume that function will always be handed a valid ticket as input
     """
+    #Based off the second hint, hash each ticket so that the starting location is the key and the destination is the value.
 
-    pass
+    #loop through the length
+    for i in range(length):
+        #insert the source and destination into hashtable
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+    #Then when constructing the entire route, ith location in the route can be found by checking the hash table for the i-1th destination
+
+    #start the first ticket source key in route to none
+    route[0] = "NONE"
+    #use a variable instead of route[0] in retrieve, location
+    location = route[0]
+
+    #loop through length - 1 so it can be within range
+    for i in range(length-1):
+        #set route at current position since the tickets source and desitnation are in hashtable 
+        route[i] = hash_table_retrieve(hashtable, location)
+        #update as you move through 
+        location = route[i]
+        
+    return route
+
